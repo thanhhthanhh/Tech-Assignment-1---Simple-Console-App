8fc4968bb7f8671026d02c823e3c64af6c5841dc
Thanh Nguyen <thanhnguyen2@usf.edu>
2021-09-07 01:10:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_1___Simple_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! My first project in ISM 4300 ");
+        }
+    }
+}
