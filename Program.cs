498a2b843ad82f7147e2ac1f42b0b4c0736ed5c4
Thanh Nguyen <thanhnguyen2@usf.edu>
2021-09-07 01:25:16 -0400

    Commit the change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Tech_Assignment_1___Simple_Console_App
         static void Main(string[] args)
         {
             Console.WriteLine("Hi! My first project in ISM 4300 ");
+
+            Console.WriteLine("Hello again!!!");
         }
     }
 }
